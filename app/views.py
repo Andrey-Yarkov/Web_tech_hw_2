4012d2fb571d173091c50846b14b747e63db52a9
Andrey-Yarkov <yarkov.andrey.v@gmail.com>
2023-11-13 10:43:40 +0300

    Realised views and urls

--- app/views.py
+++ app/views.py
@@ -1,18 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# global data
+QUESTIONS = [
+        {
+            'id': i,
+            'title': f'Question {i}',
+            'content': f'Question text question text {i}'
+        } for i in range(20)
+    ]
+
+ANSWERS = [
+        {
+            'id': i,
+            'content': f'Answer text answer text {i}'
+        } for i in range(3)
+    ]
+
+TAGS = [
+    {
+        'id': i,
+        'name': f'tag{i}'
+    } for i in range(3)
+]
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    page = request.GET.get('page', 1)
+    return render(request, 'index.html', context={'questions': QUESTIONS})
 
-def tag(request):
-    return render(request, 'tag.html')
+def tag(request, tag_name):
+    tag_item = next(item for item in TAGS if item['name'] == tag_name)
+    return render(request, 'tag.html', context={'tag': tag_item, 'questions_by_tag': QUESTIONS})
 
 def hot(request):
-    return render(request, 'hot.html')
+    return render(request, 'hot.html', context={'hot_questions': QUESTIONS})
 
-def question(request):
-    return render(request, 'question.html')
+def question(request, question_id):
+    question_item = QUESTIONS[question_id]
+    return render(request, 'question.html', context={'question': question_item, 'answers': ANSWERS})
 
 def ask(request):
     return render(request, 'ask.html')
--- askpedia_yarkov/urls.py
+++ askpedia_yarkov/urls.py
@@ -19,12 +19,12 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.index),
-    path('tag/', views.tag),
-    path('hot/', views.hot),
-    path('question/', views.question),
+    path('', views.index, name='index'),
+    path('tag/<slug:tag_name>', views.tag, name='tag'),
+    path('hot/', views.hot, name='hot'),
+    path('question/<int:question_id>', views.question, name='question'),
     path('ask/', views.ask),
-    path('login/', views.login),
-    path('signup/', views.signup),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
     path('admin/', admin.site.urls),
 ]
