01689a6a4cefb42403c7062837556dbd63446642
Andrey-Yarkov <yarkov.andrey.v@gmail.com>
2023-11-13 11:11:17 +0300

    Added  ASK button functional

--- askpedia_yarkov/urls.py
+++ askpedia_yarkov/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('tag/<slug:tag_name>', views.tag, name='tag'),
     path('hot/', views.hot, name='hot'),
     path('question/<int:question_id>', views.question, name='question'),
-    path('ask/', views.ask),
+    path('ask/', views.ask, name='ask'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('admin/', admin.site.urls),
